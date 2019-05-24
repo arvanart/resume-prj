263b7cac22a46d6a782271fffe2594dd1e4ac4f8
arvanart <esteghamat.g@gmail.com>
2019-05-25 00:45:12 +0430

    Updates

---
+++ precache-manifest.2a5706ec06a7f80a09ea07f5a8a565d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3c9acd4d7c7bcfa8d9a",
+    "url": "/arvanart/resume-prj/static/css/main.4edd1ce7.chunk.css"
+  },
+  {
+    "revision": "e3c9acd4d7c7bcfa8d9a",
+    "url": "/arvanart/resume-prj/static/js/main.1137d7fa.chunk.js"
+  },
+  {
+    "revision": "64b59c762015cfa6598d",
+    "url": "/arvanart/resume-prj/static/js/runtime~main.47fbeb0a.js"
+  },
+  {
+    "revision": "ba9dc4325d2dae1029de",
+    "url": "/arvanart/resume-prj/static/js/2.1c29d6d4.chunk.js"
+  },
+  {
+    "revision": "bcbbcbd615e58e5cc75da0182dd9531a",
+    "url": "/arvanart/resume-prj/index.html"
+  }
+];
